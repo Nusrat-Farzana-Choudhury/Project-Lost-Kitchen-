086325b0346884a0081fac5f43b723ec31273864
Nusrat Farzana Choudhury <105939501+Nusrat-Farzana-Choudhury@users.noreply.github.com>
2023-11-29 19:38:05 +0600

    About us (page01)

---
+++ AboutUs.java
@@ -0,0 +1,15 @@
+public class AboutUs {
+    public void aboutUs() {
+        System.out.println("\n\n\t\t\t\t\t\t\t\t\t\t\t---------------------About Us---------------------\n");
+        System.out.println("\t\t\t\t\t\t\t\t\t\t\t\t\t-We are always here to serve you\n\n");
+        System.out.println("----------Our story----------\n");
+        System.out.println("Our system is specially made for resident students of Daffodil International University who live in Ashuliya.\nIn our university, a large number of students come from far away.They stay in the hall or live on rent nearby.\nMany of them eat at different restaurants daily.\nWhile having food they face some challenges regularly such as consuming unhealthy foods, having a poor diet, safety issues for females, and unreasonable costs.\n\t-These thoughts led us to create a homemade food delivery service named 'LOST KITCHEN'.\n\tIt will bring back the taste, which they used to get from the kitchen of their home right next to their doorsteps within a few clicks.\n\n"); 
+        System.out.println("----------Our Mission----------\n");
+        System.out.println("We are committed to providing you with-\n\t~Hygenic food made with care\n\t~Food made with fresh ingredients\n\t~Fast delivery with no delivery cost at all\n\t~Offer packages maintaining proper diet chart by a certified nutritionist\n\n");
+        System.out.println("***OUR SERVICES ARE ONLY AVAILABLE IN ASHULIYA & JUST FOR THE STUDENT OF DAFFODIL INTERNATIONAL UNIVERSITY.***\n\n");
+        System.out.println("----------Our team----------\n");
+        System.out.println("Our team is consisted of a group of courageous people who have made our service successful-\n\n\t||Founder:\n\t\t~Ms. Nusrat Farzana Choudhury\n\t\t Founder, Lost Kitchen\n\n\t||Management Team:\n\t\t~Ms. Dr. Tasnim Zara\n\t\t Nutritionist, Lost Kitchen\n\t\t~Mr. Abin Mahmud Khan\n\t\t Manager, Lost Kitchen\n\t\t~Mr. Jamshed Uddin\n\t\t Supervisor, Lost Kitchen\n\t\t~Ms. Sheuly Khanam\n\t\t Customer support, Lost Kitchen\n\n\t||Delivery service:\n\t\t~SteadFast\n\n");
+        System.out.println("----------Contact Us----------\n");
+        System.out.println("Address-\n18/A, Prantik Road,\nCharabagh, Ashuliya,\nDhaka.\n\nPhone number-\n\t~ +88 01423272703\n\t~ +02 436183543\n\nE-mail:\nlostkitchensupport@gmail.com\n\n");
+    }
+}
