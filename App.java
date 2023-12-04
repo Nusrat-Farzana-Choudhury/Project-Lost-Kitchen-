219b4b725caf057bef77e5dea01fd3793b07ead4
Nusrat Farzana Choudhury <105939501+Nusrat-Farzana-Choudhury@users.noreply.github.com>
2023-12-04 18:30:22 +0600

    Driver code

--- App.java
+++ App.java
@@ -2,7 +2,7 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class App {
-
+    
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
