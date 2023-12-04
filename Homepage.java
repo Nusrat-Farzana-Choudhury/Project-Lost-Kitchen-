b94f4d17fbc697cb0e1b6140193a046e024545f3
Nusrat Farzana Choudhury <105939501+Nusrat-Farzana-Choudhury@users.noreply.github.com>
2023-12-04 18:49:11 +0600

    Update Homepage.java

--- Homepage.java
+++ Homepage.java
@@ -1,29 +1,22 @@
 import java.util.Scanner;
-public class Homepage{
-    private int choice;
 
-    public int getChoice() {
-        return choice;
-    }
-    public void setChoice(int choice){
-        this.choice = choice;
+public class Homepage {
+    int choice;
+   public void displayMenu(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("\n\n\t\t\t\t\t\t\t\t\t\tWelcome to\n");
+        System.out.println("\t\t\t\t\t\t\t\t\t\t----------LOST KITCHEN-------------\n");
+        System.out.println("\t\t\t\t\t\t\t\t\t\t\t-Where every flavor tells a story\n\n\n");
+        System.out.println("1. About us");
+        System.out.println("2. User guides");
+        System.out.println("3. Register/Log in");
+        System.out.println("4. Order now!");
+        System.out.println("5. Hot deals!!!");
+        System.out.println("6. FAQs");
+        System.out.println("7. Review");
+        System.out.println("8. Exit");
+        System.out.print("\n\nEnter your choice: ");
+        choice = scanner.nextInt();
     }
 
-    public void displayMenu() {
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("\n\n\t\t\t\t\t\t\t\t\t\tWelcome to\n");
-            System.out.println("\t\t\t\t\t\t\t\t\t\t----------LOST KITCHEN-------------\n");
-            System.out.println("\t\t\t\t\t\t\t\t\t\t\t-Where every flavor tells a story\n\n\n");
-            System.out.println("1. About us");
-            System.out.println("2. User guides");
-            System.out.println("3. Register/Log in");
-            System.out.println("4. Order now!");
-            System.out.println("5. Hot deals!!!");
-            System.out.println("6. FAQs");
-            System.out.println("7. Review");
-            System.out.println("8. Exit");
-            System.out.print("\n\nEnter your choice: ");
-            choice = scanner.nextInt();
-            scanner.close();
-        }
-    }
+}
